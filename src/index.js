2315e9d96379d7c94624d6c328577fb97d816117
YourNewFriendCharlie <Yournewfriendcharlie@gmail.com>
2025-06-20 11:43:45 -0400

    Fixed index.js file - restored proper JavaScript code and updated browser title

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-< !DOCTYPE html >
-  <html lang="en">
-    <head>
-      <meta charset="utf-8" />
-      <link rel="icon" href="data:text/plain;charset=utf-8,🍸" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#000000" />
-      <meta name="description" content="Professional beverage recipe costing and pricing calculator by MargaritaHotSauceLLC" />
-      <link rel="apple-touch-icon" href="data:text/plain;charset=utf-8,🍸" />
-      <title>Beverage Metrics Tool</title>
-    </head>
-    <body>
-      <noscript>You need to enable JavaScript to run this app.</noscript>
-      <div id="root"></div>
-    </body>
-  </html>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
